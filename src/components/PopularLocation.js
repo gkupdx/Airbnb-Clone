2dbcdb84823c2d84f8ed6e908c8d3cb3f2e2071f
Grady Ku <gkupdx@gmail.com>
2021-11-05 13:49:56 -0700

    Add PopularLocation.js
    - create new PopularLocation component that displays location content 
    for 'popular' destinations

---
+++ src/components/PopularLocation.js
@@ -0,0 +1,117 @@
+//// PopularLocation.js - for 'Popular destinations' button click
+
+const PopularLocation = () => {
+  const lightText = {
+    color: "rgba(0, 0, 0, 0.6)"
+  }
+
+  return (
+    <div className="popularLocation">
+      <div className="popularWrapper">
+        <div className="columnOne">
+          <button>Canmore<br></br><span style={lightText}>Alberta</span></button>
+          <button>Prescott<br></br><span style={lightText}>Arizona</span></button>
+          <button>Mountain View<br></br><span style={lightText}>Arkansas</span></button>
+          <button>Anaheim<br></br><span style={lightText}>California</span></button>
+          <button>Sonoma<br></br><span style={lightText}>California</span></button>
+          <button>Brighton<br></br><span style={lightText}>England</span></button>
+          <button>South West England<br></br><span style={lightText}>England</span></button>
+          <button>Kissimmee<br></br><span style={lightText}>Florida</span></button>
+          <button>St. Augustine<br></br><span style={lightText}>Florida</span></button>
+          <button>Mykonos<br></br><span style={lightText}>Greece</span></button>
+          <button>Bar Harbor<br></br><span style={lightText}>Maine</span></button>
+          <button>Las Vegas<br></br><span style={lightText}>Nevada</span></button>
+          <button>Evans Head<br></br><span style={lightText}>New South Wales</span></button>
+          <button>Florence<br></br><span style={lightText}>Oregon</span></button>
+          <button>Maleny<br></br><span style={lightText}>Queensland</span></button>
+          <button>Scottish Highlands<br></br><span style={lightText}>Scotland</span></button>
+          <button>Charleston<br></br><span style={lightText}>South Carolina</span></button>
+          <button>Concan<br></br><span style={lightText}>Texas</span></button>
+          <button>Waco<br></br><span style={lightText}>Texas</span></button>
+          <button>Manchester<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Wells-next-the-Sea<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Castlemaine<br></br><span style={lightText}>Victoria</span></button>
+          <button>Chincoteague<br></br><span style={lightText}>Virginia</span></button>
+          <button>Tenby<br></br><span style={lightText}>Wales</span></button>
+        </div>
+        <div className="columnTwo">
+          <button>Benalmadena<br></br><span style={lightText}>Andalusia</span></button>
+          <button>Scottsdale<br></br><span style={lightText}>Arizona</span></button>
+          <button>Devonport<br></br><span style={lightText}>Australia</span></button>
+          <button>Monterey<br></br><span style={lightText}>California</span></button>
+          <button>La Serena<br></br><span style={lightText}>Coquimbo</span></button>
+          <button>Bude<br></br><span style={lightText}>England</span></button>
+          <button>Whitby<br></br><span style={lightText}>England</span></button>
+          <button>Longboat Key<br></br><span style={lightText}>Florida</span></button>
+          <button>The Villages<br></br><span style={lightText}>Florida</span></button>
+          <button>Santorini<br></br><span style={lightText}>Greece</span></button>
+          <button>Mackinac Island<br></br><span style={lightText}>Michigan</span></button>
+          <button>Madrid<br></br><span style={lightText}>New Mexico</span></button>
+          <button>Sawtell<br></br><span style={lightText}>New South Wales</span></button>
+          <button>Lakeside<br></br><span style={lightText}>Oregon</span></button>
+          <button>Stanthorpe<br></br><span style={lightText}>Queensland</span></button>
+          <button>St Andrews<br></br><span style={lightText}>Scotland</span></button>
+          <button>Hvar<br></br><span style={lightText}>Split-Dalmatia County</span></button>
+          <button>Fredericksburg<br></br><span style={lightText}>Texas</span></button>
+          <button>Forest of Dean District<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Seaview<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>St. George<br></br><span style={lightText}>Utah</span></button>
+          <button>Healesville<br></br><span style={lightText}>Victoria</span></button>
+          <button>Williamsburg<br></br><span style={lightText}>Victoria</span></button>
+        </div>
+        <div className="columnThree">
+          <button>Marbella<br></br><span style={lightText}>Andalusia</span></button>
+          <button>Tucson<br></br><span style={lightText}>Arizona</span></button>
+          <button>Mallacoota<br></br><span style={lightText}>Australia</span></button>
+          <button>Paso Robles<br></br><span style={lightText}>California</span></button>
+          <button>Dubai<br></br><span style={lightText}>Dubai</span></button>
+          <button>Newcastle upon Tyne<br></br><span style={lightText}>England</span></button>
+          <button>Fort Myers<br></br><span style={lightText}>Florida</span></button>
+          <button>Orlando<br></br><span style={lightText}>Florida</span></button>
+          <button>Dahlonega<br></br><span style={lightText}>Georgia</span></button>
+          <button>Oahu<br></br><span style={lightText}>Hawaii</span></button>
+          <button>St. Joseph<br></br><span style={lightText}>Michigan</span></button>
+          <button>Santa Fe<br></br><span style={lightText}>New Mexico</span></button>
+          <button>Young<br></br><span style={lightText}>New South Wales</span></button>
+          <button>Lincoln City<br></br><span style={lightText}>Oregon</span></button>
+          <button>Newport<br></br><span style={lightText}>Rhode Island</span></button>
+          <button>McLaren Vale<br></br><span style={lightText}>South Australia</span></button>
+          <button>Saint John<br></br><span style={lightText}>St. John</span></button>
+          <button>New Braunfels<br></br><span style={lightText}>Texas</span></button>
+          <button>Jersey<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Southwold<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Benidorm<br></br><span style={lightText}>Valencian Community</span></button>
+          <button>Marysville<br></br><span style={lightText}>Victoria</span></button>
+          <button>Hay-on-Wye<br></br><span style={lightText}>Wales</span></button>
+        </div>
+        <div className="columnFour">
+          <button>Mijas<br></br><span style={lightText}>Andalusia</span></button>
+          <button>Jasper<br></br><span style={lightText}>Arkansas</span></button>
+          <button>Ibiza<br></br><span style={lightText}>Balearic islands</span></button>
+          <button>Santa Barbara<br></br><span style={lightText}>California</span></button>
+          <button>Birmingham<br></br><span style={lightText}>England</span></button>
+          <button>Padstow<br></br><span style={lightText}>England</span></button>
+          <button>Jacksonville<br></br><span style={lightText}>Florida</span></button>
+          <button>St Peterburg<br></br><span style={lightText}>Florida</span></button>
+          <button>Crete<br></br><span style={lightText}>Greece</span></button>
+          <button>Capri<br></br><span style={lightText}>Italy</span></button>
+          <button>Larsmont<br></br><span style={lightText}>Minnesota</span></button>
+          <button>Bermagui<br></br><span style={lightText}>New South Wales</span></button>
+          <button>Ocracoke<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Paphos<br></br><span style={lightText}>Paphos</span></button>
+          <button>Glasgow<br></br><span style={lightText}>Scotland</span></button>
+          <button>Wallaroo<br></br><span style={lightText}>South Australia</span></button>
+          <button>Chattanooga<br></br><span style={lightText}>Tennessee</span></button>
+          <button>Rockport<br></br><span style={lightText}>Texas</span></button>
+          <button>Lyme Regis<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Staithes<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Barwon Heads<br></br><span style={lightText}>Victoria</span></button>
+          <button>Sorrento<br></br><span style={lightText}>Victoria</span></button>
+          <button>Llandudno<br></br><span style={lightText}>Wales</span></button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default PopularLocation;
