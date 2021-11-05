9cf4a70cff3343efa5e7aca5da5739f1969eb91c
Grady Ku <gkupdx@gmail.com>
2021-11-05 13:48:35 -0700

    Add BeachLocation.js
    - create new BeachLocation component that displays location content for 
    'beach' destinations

---
+++ src/components/BeachLocation.js
@@ -0,0 +1,183 @@
+//// BeachLocation.js - for 'Beach destinations' button click
+
+const BeachLocation = () => {
+  const lightText = {
+    color: "rgba(0, 0, 0, 0.6)"
+  }
+  
+  return (
+    <div className="beachLocation">
+      <div className="beachWrapper">
+        <div className="columnOne">
+          <button>Dauphin Island<br></br><span style={lightText}>Alabama</span></button>
+          <button>Crescent Head<br></br><span style={lightText}>Australia</span></button>
+          <button>Melbourne Beach<br></br><span style={lightText}>Australia</span></button>
+          <button>Big Sur<br></br><span style={lightText}>California</span></button>
+          <button>Huntington Beach<br></br><span style={lightText}>California</span></button>
+          <button>Malibu<br></br><span style={lightText}>California</span></button>
+          <button>Palm Springs<br></br><span style={lightText}>California</span></button>
+          <button>Sea Ranch<br></br><span style={lightText}>California</span></button>
+          <button>Tenerife<br></br><span style={lightText}>Canary Islands</span></button>
+          <button>Blackpool<br></br><span style={lightText}>England</span></button>
+          <button>Albufeira<br></br><span style={lightText}>Faro District</span></button>
+          <button>Bradenton<br></br><span style={lightText}>Florida</span></button>
+          <button>Cocoa Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Fort Lauderdale<br></br><span style={lightText}>Florida</span></button>
+          <button>Gulf of Mexico<br></br><span style={lightText}>Florida</span></button>
+          <button>Key Largo<br></br><span style={lightText}>Florida</span></button>
+          <button>Mexico Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Naples<br></br><span style={lightText}>Florida</span></button>
+          <button>Panama City Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Saint George Island<br></br><span style={lightText}>Florida</span></button>
+          <button>Seaside<br></br><span style={lightText}>Florida</span></button>
+          <button>Tampa<br></br><span style={lightText}>Florida</span></button>
+          <button>Honolulu<br></br><span style={lightText}>Hawaii</span></button>
+          <button>Kihei<br></br><span style={lightText}>Hawaii</span></button>
+          <button>Old Orchard Beach<br></br><span style={lightText}>Maine</span></button>
+          <button>Biloxi<br></br><span style={lightText}>Mississippi</span></button>
+          <button>Ocean City<br></br><span style={lightText}>New Jersey</span></button>
+          <button>Fire Island<br></br><span style={lightText}>New York</span></button>
+          <button>Carolina Beach<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Kure Beach<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Surf City<br></br><span style={lightText}>north Carolina</span></button>
+          <button>Sauble Beach<br></br><span style={lightText}>Ontario</span></button>
+          <button>Carvoeiro<br></br><span style={lightText}>Portugal</span></button>
+          <button>Puerto Ponasco<br></br><span style={lightText}>Sonora</span></button>
+          <button>Fripp Island<br></br><span style={lightText}>South Carolina</span></button>
+          <button>Pawleys Island<br></br><span style={lightText}>South Carolina</span></button>
+          <button>Alicante<br></br><span style={lightText}>Spain</span></button>
+          <button>Los Cristianos<br></br><span style={lightText}>Spain</span></button>
+          <button>Surfside Beach<br></br><span style={lightText}>Texas</span></button>
+          <button>Abersoch<br></br><span style={lightText}>Wales</span></button>
+        </div>
+        <div className="columnTwo">
+          <button>Fort Morgan<br></br><span style={lightText}>Alabama</span></button>
+          <button>Gerringong<br></br><span style={lightText}>Australia</span></button>
+          <button>Moonta Bay<br></br><span style={lightText}>Australia</span></button>
+          <button>Bodega Bay<br></br><span style={lightText}>California</span></button>
+          <button>La Jolla Shores Beach<br></br><span style={lightText}>California</span></button>
+          <button>Mission Beach<br></br><span style={lightText}>California</span></button>
+          <button>Pismo Beach<br></br><span style={lightText}>California</span></button>
+          <button>Playa Blanca<br></br><span style={lightText}>Canarias</span></button>
+          <button>Ayia Napa<br></br><span style={lightText}>Cyprus</span></button>
+          <button>Isle of Wight<br></br><span style={lightText}>England</span></button>
+          <button>Alys Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Cape San Blas<br></br><span style={lightText}>Florida</span></button>
+          <button>Daytona Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Fort Myers Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Indian Rocks Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Madeira Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Miami Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Navarre Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Penascola Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Santa Rosa Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>South Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>West Palm Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Kailua-Kona<br></br><span style={lightText}>Hawaii</span></button>
+          <button>Ko Olina Beach<br></br><span style={lightText}>Hawaii</span></button>
+          <button>Wells<br></br><span style={lightText}>Maine</span></button>
+          <button>Avalon<br></br><span style={lightText}>New Jersey</span></button>
+          <button>Sea Isle City<br></br><span style={lightText}>New Jersey</span></button>
+          <button>Montauk<br></br><span style={lightText}>New York</span></button>
+          <button>Corolla<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Nags Head<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Wilmington<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Wasaga Beach<br></br><span style={lightText}>Ontario</span></button>
+          <button>Gold Coast<br></br><span style={lightText}>Queensland</span></button>
+          <button>Charleston Oceanfront Villas, Folly Beach<br></br><span style={lightText}>South Carolina</span></button>
+          <button>Isle of Palms<br></br><span style={lightText}>South Carolina</span></button>
+          <button>Seabrook Island<br></br><span style={lightText}>South Carolina</span></button>
+          <button>Costa Adeje<br></br><span style={lightText}>Spain</span></button>
+          <button>Corpus Christi<br></br><span style={lightText}>Texas</span></button>
+          <button>Bournemouth<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Seabrook<br></br><span style={lightText}>Washington</span></button>
+        </div>
+        <div className="columnThree">
+          <button>Gulf Shores<br></br><span style={lightText}>Alabama</span></button>
+          <button>Hamilton Island<br></br><span style={lightText}>Australia</span></button>
+          <button>Ocean Grove<br></br><span style={lightText}>Australia</span></button>
+          <button>Cambria<br></br><span style={lightText}>California</span></button>
+          <button>Laguna Beach<br></br><span style={lightText}>California</span></button>
+          <button>Newport Beach<br></br><span style={lightText}>California</span></button>
+          <button>Santa Cruz<br></br><span style={lightText}>California</span></button>
+          <button>Fuerteventura<br></br><span style={lightText}>Canary Islands</span></button>
+          <button>Bethany Beach<br></br><span style={lightText}>Delaware</span></button>
+          <button>Newquay<br></br><span style={lightText}>England</span></button>
+          <button>Anna Maria Island<br></br><span style={lightText}>Florida</span></button>
+          <button>Captiva<br></br><span style={lightText}>Florida</span></button>
+          <button>Destin<br></br><span style={lightText}>Florida</span></button>
+          <button>Fort Walton Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Islamorada<br></br><span style={lightText}>Florida</span></button>
+          <button>Marathon<br></br><span style={lightText}>Florida</span></button>
+          <button>Miami<br></br><span style={lightText}>Florida</span></button>
+          <button>New Smyrna Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Perdido Key<br></br><span style={lightText}>Florida</span></button>
+          <button>Sarasota<br></br><span style={lightText}>Florida</span></button>
+          <button>St. Augustine Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Bora Bora<br></br><span style={lightText}>French Polynesia</span></button>
+          <button>Kapalua<br></br><span style={lightText}>Hawaii</span></button>
+          <button>Maui<br></br><span style={lightText}>Hawaii</span></button>
+          <button>Cape Cod<br></br><span style={lightText}>Massachusetts</span></button>
+          <button>Jersey Shore<br></br><span style={lightText}>New Jersey</span></button>
+          <button>Stone Harbor<br></br><span style={lightText}>New Jersey</span></button>
+          <button>Ocean Beach<br></br><span style={lightText}>New York</span></button>
+          <button>Holden Beach<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Ocean Isle Beach<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Wrightsville Beach<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Cannon Beach<br></br><span style={lightText}>Oregon</span></button>
+          <button>Moreton Island<br></br><span style={lightText}>Queensland</span></button>
+          <button>Edisto Beach<br></br><span style={lightText}>South Carolina</span></button>
+          <button>Kiawah Island<br></br><span style={lightText}>South Carolina</span></button>
+          <button>Sullivan's Island<br></br><span style={lightText}>South Carolina</span></button>
+          <button>Costa Del Sol<br></br><span style={lightText}>Spain</span></button>
+          <button>Port Aransas<br></br><span style={lightText}>Texas</span></button>
+          <button>New Quay<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Jurien Bay<br></br><span style={lightText}>Western Australia</span></button>
+        </div>
+        <div className="columnFour">
+          <button>Bruny Island<br></br><span style={lightText}>Australia</span></button>
+          <button>Lancelin<br></br><span style={lightText}>Australia</span></button>
+          <button>Majorca<br></br><span style={lightText}>Balearic Islands</span></button>
+          <button>Cayucos<br></br><span style={lightText}>California</span></button>
+          <button>Long Beach<br></br><span style={lightText}>California</span></button>
+          <button>Oceanside<br></br><span style={lightText}>California</span></button>
+          <button>Santa Monica<br></br><span style={lightText}>California</span></button>
+          <button>Puerto del Carmen<br></br><span style={lightText}>Canary Islands</span></button>
+          <button>Dewey Beach<br></br><span style={lightText}>Delaware</span></button>
+          <button>Weymouth<br></br><span style={lightText}>England</span></button>
+          <button>Boca Grande<br></br><span style={lightText}>Florida</span></button>
+          <button>Clearwater Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Englewood<br></br><span style={lightText}>Florida</span></button>
+          <button>Grayton Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Jacksonville Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Marco Island<br></br><span style={lightText}>Florida</span></button>
+          <button>Miramar Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Okaloosa Island<br></br><span style={lightText}>Florida</span></button>
+          <button>Rosemary Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Seagrove Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>St. Pete Beach<br></br><span style={lightText}>Florida</span></button>
+          <button>Tybee Island<br></br><span style={lightText}>Georgia</span></button>
+          <button>Kauai<br></br><span style={lightText}>Hawaii</span></button>
+          <button>Topsail Island<br></br><span style={lightText}>Hawaii</span></button>
+          <button>Nantucket<br></br><span style={lightText}>Massachusetts</span></button>
+          <button>North Wildwood<br></br><span style={lightText}>New Jersey</span></button>
+          <button>Hyams Beach<br></br><span style={lightText}>New South Wales</span></button>
+          <button>Atlantic Beach<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Kitty Hawk<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Sunset Beach<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Grand Bend<br></br><span style={lightText}>Ontario</span></button>
+          <button>Seaside<br></br><span style={lightText}>Oregon</span></button>
+          <button>Block Island<br></br><span style={lightText}>Rhode Island</span></button>
+          <button>Folly Beach<br></br><span style={lightText}>South Carolina</span></button>
+          <button>Myrtle Beach<br></br><span style={lightText}>South Carolina</span></button>
+          <button>Surfside Beach<br></br><span style={lightText}>South Carolina</span></button>
+          <button>Fuengirola<br></br><span style={lightText}>Spain</span></button>
+          <button>South Padre Island<br></br><span style={lightText}>Texas</span></button>
+          <button>Anglesea<br></br><span style={lightText}>Victoria</span></button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BeachLocation;
