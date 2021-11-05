a955add50775f69ac868f98128627823677c23a1
Grady Ku <gkupdx@gmail.com>
2021-11-05 13:49:34 -0700

    Add OutdoorLocation.js
    - create new OutdoorLocation component that displays location content 
    for 'outdoor adventure' destinations

---
+++ src/components/OutdoorLocation.js
@@ -0,0 +1,149 @@
+//// OutdoorLocation.js - for 'Destinations for outdoor adventure' button click
+
+const OutdoorLocation = () => {
+  const lightText = {
+    color: "rgba(0, 0, 0, 0.6)"
+  }
+
+  return (
+    <div className="outdoorLocation">
+      <div className="outdoorWrapper">
+        <div className="columnOne">
+          <button>Lake Martin<br></br><span style={lightText}>Alabama</span></button>
+          <button>Lake Havasu City<br></br><span style={lightText}>Arizona</span></button>
+          <button>Pinetop-Lakeside<br></br><span style={lightText}>Arizona</span></button>
+          <button>Emerald Lake<br></br><span style={lightText}>British Columbia</span></button>
+          <button>Mammoth Lakes<br></br><span style={lightText}>California</span></button>
+          <button>Cultus Lake<br></br><span style={lightText}>Canada</span></button>
+          <button>The Blue Mountains<br></br><span style={lightText}>Canada</span></button>
+          <button>Castle Hill<br></br><span style={lightText}>Canterbury</span></button>
+          <button>Durango<br></br><span style={lightText}>Colorado</span></button>
+          <button>Vail<br></br><span style={lightText}>Colorado</span></button>
+          <button>Cape Coral<br></br><span style={lightText}>Florida</span></button>
+          <button>Corfu<br></br><span style={lightText}>Greece</span></button>
+          <button>Lake Cumberland<br></br><span style={lightText}>Kentucky</span></button>
+          <button>Lake Michigan Beach<br></br><span style={lightText}>Michigan</span></button>
+          <button>Upper Peninsula of Michigan<br></br><span style={lightText}>Michigan</span></button>
+          <button>West Yellowstone<br></br><span style={lightText}>Montana</span></button>
+          <button>Bellingen<br></br><span style={lightText}>New South Wales</span></button>
+          <button>Niagara Falls<br></br><span style={lightText}>New York</span></button>
+          <button>Lake Gaston<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Mountain<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Turner Falls<br></br><span style={lightText}>Oklahoma</span></button>
+          <button>Bend<br></br><span style={lightText}>Oregon</span></button>
+          <button>Lake Harmony<br></br><span style={lightText}>Pennsylvania</span></button>
+          <button>Isle of Mull<br></br><span style={lightText}>Scotland</span></button>
+          <button>Big Bend National Park<br></br><span style={lightText}>Texas</span></button>
+          <button>Betws-y-Coed<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Port Isaac<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Inverloch<br></br><span style={lightText}>Victoria</span></button>
+          <button>Wilsons Promontory<br></br><span style={lightText}>Victoria</span></button>
+          <button>Lake Chelan<br></br><span style={lightText}>Washington</span></button>
+          <button>Skamania<br></br><span style={lightText}>Washington</span></button>
+          <button>Yellowstone National Park<br></br><span style={lightText}>Wyoming</span></button>
+        </div>
+        <div className="columnTwo">
+          <button>Banff<br></br><span style={lightText}>Alberta</span></button>
+          <button>Lake Powell<br></br><span style={lightText}>Arizona</span></button>
+          <button>Red Rock<br></br><span style={lightText}>Arizona</span></button>
+          <button>Vancouver Island<br></br><span style={lightText}>British Columbia</span></button>
+          <button>Palm Desert<br></br><span style={lightText}>California</span></button>
+          <button>Georgian Bay<br></br><span style={lightText}>Canada</span></button>
+          <button>West Kelowna<br></br><span style={lightText}>Canada</span></button>
+          <button>Aspen<br></br><span style={lightText}>Colorado</span></button>
+          <button>Estes Park<br></br><span style={lightText}>Colorado</span></button>
+          <button>Winter Park<br></br><span style={lightText}>Colorado</span></button>
+          <button>Blue Ridge<br></br><span style={lightText}>Georgia</span></button>
+          <button>McCall<br></br><span style={lightText}>Idaho</span></button>
+          <button>Portland<br></br><span style={lightText}>Maine</span></button>
+          <button>Lakeside<br></br><span style={lightText}>Michigan</span></button>
+          <button>Branson<br></br><span style={lightText}>Missouri</span></button>
+          <button>Mount Charleston<br></br><span style={lightText}>Nevada</span></button>
+          <button>Brunswick Heads<br></br><span style={lightText}>New South Wales</span></button>
+          <button>Bald Head Island<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Lake Lure<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Oak Island<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Algonquin Park<br></br><span style={lightText}>Ontario</span></button>
+          <button>Crater Lake<br></br><span style={lightText}>Oregon</span></button>
+          <button>Mount Pocono<br></br><span style={lightText}>Pennsylvania</span></button>
+          <button>Robe<br></br><span style={lightText}>South Australia</span></button>
+          <button>Canyon Lake<br></br><span style={lightText}>Texas</span></button>
+          <button>Filey<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Moab<br></br><span style={lightText}>Utah</span></button>
+          <button>Metung<br></br><span style={lightText}>Victoria</span></button>
+          <button>Shenandoah<br></br><span style={lightText}>Virginia</span></button>
+          <button>Lake Crescent<br></br><span style={lightText}>Washington</span></button>
+          <button>Snowshoe<br></br><span style={lightText}>West Virginia</span></button>
+        </div>
+        <div className="columnThree">
+          <button>Nerja<br></br><span style={lightText}>Andalucia</span></button>
+          <button>North Rim<br></br><span style={lightText}>Arizona</span></button>
+          <button>Dinner Plain<br></br><span style={lightText}>Australia</span></button>
+          <button>Victoria<br></br><span style={lightText}>British Columbia</span></button>
+          <button>Shaver Lake<br></br><span style={lightText}>California</span></button>
+          <button>Manitoulin Island<br></br><span style={lightText}>Canada</span></button>
+          <button>Gran Canaria<br></br><span style={lightText}>Canary Islands</span></button>
+          <button>Colorado Springs<br></br><span style={lightText}>Colorado</span></button>
+          <button>Grand Lake<br></br><span style={lightText}>Colorado</span></button>
+          <button>Salcombe<br></br><span style={lightText}>England</span></button>
+          <button>Jekyll Island<br></br><span style={lightText}>Georgia</span></button>
+          <button>Clear Lake<br></br><span style={lightText}>Iowa</span></button>
+          <button>South Portland<br></br><span style={lightText}>Maine</span></button>
+          <button>Torch Lake<br></br><span style={lightText}>Michigan</span></button>
+          <button>Lake of the Ozarks<br></br><span style={lightText}>Missouri</span></button>
+          <button>Cloudcroft<br></br><span style={lightText}>New Mexico</span></button>
+          <button>Adirondack Mountains<br></br><span style={lightText}>New York</span></button>
+          <button>Bryson City<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Lake Norman of Catawba<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Geneva-on-the-Lake<br></br><span style={lightText}>Ohio</span></button>
+          <button>Muskoka Lakes<br></br><span style={lightText}>Ontario</span></button>
+          <button>Northern Oregon Coast Range<br></br><span style={lightText}>Oregon</span></button>
+          <button>Mont-Tremblant<br></br><span style={lightText}>Quebec</span></button>
+          <button>Mountain Rest<br></br><span style={lightText}>South Carolina</span></button>
+          <button>Lake Austin<br></br><span style={lightText}>Texas</span></button>
+          <button>Fort Williams<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Mount Zion<br></br><span style={lightText}>Utah</span></button>
+          <button>Mount Buller<br></br><span style={lightText}>Victoria</span></button>
+          <button>Smith Mountain Lake<br></br><span style={lightText}>Virginia</span></button>
+          <button>Lake Quinault<br></br><span style={lightText}>Washington</span></button>
+          <button>Lake Geneva<br></br><span style={lightText}>Wisconsin</span></button>
+        </div>
+        <div className="columnFour">
+          <button>Greer<br></br><span style={lightText}>Arizona</span></button>
+          <button>Payson<br></br><span style={lightText}>Arizona</span></button>
+          <button>Streaky Bay<br></br><span style={lightText}>Australia</span></button>
+          <button>Idyllwild-Pine Cove<br></br><span style={lightText}>California</span></button>
+          <button>South Lake Tahoe<br></br><span style={lightText}>California</span></button>
+          <button>Ottawa River<br></br><span style={lightText}>Canada</span></button>
+          <button>Lanzarote<br></br><span style={lightText}>Canary Islands</span></button>
+          <button>Denver<br></br><span style={lightText}>Colorado</span></button>
+          <button>Keystone<br></br><span style={lightText}>Colorado</span></button>
+          <button>Swanage<br></br><span style={lightText}>England</span></button>
+          <button>Lake Lanier<br></br><span style={lightText}>Georgia</span></button>
+          <button>Lough Eske<br></br><span style={lightText}>Ireland</span></button>
+          <button>Deep Creek Lake<br></br><span style={lightText}>Maryland</span></button>
+          <button>Traverse City<br></br><span style={lightText}>Michigan</span></button>
+          <button>Big Sky<br></br><span style={lightText}>Montana</span></button>
+          <button>Red River<br></br><span style={lightText}>New Mexico</span></button>
+          <button>Lake Placid<br></br><span style={lightText}>New York</span></button>
+          <button>Emerald Isle<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Mount Airy<br></br><span style={lightText}>North Carolina</span></button>
+          <button>Medicine Park<br></br><span style={lightText}>Oklahoma</span></button>
+          <button>Tobermory<br></br><span style={lightText}>Ontario</span></button>
+          <button>Sunriver<br></br><span style={lightText}>Oregon</span></button>
+          <button>Aviemore<br></br><span style={lightText}>Scotland</span></button>
+          <button>St Helens<br></br><span style={lightText}>Tasmania</span></button>
+          <button>Lake Buchanan<br></br><span style={lightText}>Texas</span></button>
+          <button>Loch Lomond<br></br><span style={lightText}>United Kingdom</span></button>
+          <button>Moraira<br></br><span style={lightText}>Valencian Community</span></button>
+          <button>Port Campbell<br></br><span style={lightText}>Victoria</span></button>
+          <button>Snowdon<br></br><span style={lightText}>Wales</span></button>
+          <button>Quinault<br></br><span style={lightText}>Washington</span></button>
+          <button>Wisconsin Dells<br></br><span style={lightText}>Wisconsin</span></button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default OutdoorLocation;
