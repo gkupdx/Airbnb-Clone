0eef674852ddedd26cef69cd5899f269bf4b455a
Grady Ku <gkupdx@gmail.com>
2021-11-06 11:54:13 -0700

    Add Discover.js
    - create new Discover component that displays the 'Discover things to 
    do' section

---
+++ src/components/Discover.js
@@ -0,0 +1,28 @@
+//// Discover.js - 'Discover things to do' section
+
+const Discover = () => {
+  return (
+    <div className="discover">
+      <h2>Discover things to do</h2>
+      <div className="discoverWrapper">
+        <div>
+          <img src="https://a0.muscache.com/im/pictures/ad109d56-2421-40cd-98e6-e114160dc85b.jpg?im_w=480" alt="Sheep"/>
+          <h3>Experiences</h3>
+          <p>Find unforgettable activities near you.</p>
+        </div>
+        <div>
+          <img src="https://a0.muscache.com/im/pictures/0ce799cb-7553-4369-be9e-d0011e0ef636.jpg?im_w=480" alt="Tablet"/>
+          <h3>Online Experiences</h3>
+          <p>Live, interactive activities led by Hosts.</p>
+        </div>
+        <div>
+          <img src="https://a0.muscache.com/im/pictures/3c2676df-0874-45a3-a82f-bbf57ccde1cc.jpg?im_w=480" alt="Canoe"/>
+          <h3>Outdoor collection</h3>
+          <p>Experiences that immerse you in nature.</p>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Discover;
