d89d96433b6795ecfe2c227c78e3912223c9e2df
Grady Ku <gkupdx@gmail.com>
2021-11-06 11:53:22 -0700

    Update App.js
    - import newly added components
    - render new components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 //// App.js
 
+import LiveAnywhere from './components/LiveAnywhere';
+import Hosting from './components/Hosting';
+import Discover from './components/Discover';
 import Inspiration from './components/Inspiration';
 import Footer from './components/Footer';
 
@@ -7,6 +10,9 @@ function App() {
 
   return (
     <div className="container">
+      <LiveAnywhere />
+      <Hosting />
+      <Discover />
       <Inspiration />
       <Footer />
     </div>
