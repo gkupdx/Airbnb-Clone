e1fa093a3adf1b8a918b4bda11d3807298276980
Grady Ku <gkupdx@gmail.com>
2021-11-06 11:54:45 -0700

    Add Hosting.js
    - create new Hosting component that displays the 'Try hosting' image 
    banner

---
+++ src/components/Hosting.js
@@ -0,0 +1,18 @@
+//// Hosting.js - 'Try hosting' section
+
+const Hosting = () => {
+  return (
+    <div className="hosting">
+      <div className="hostingWrapper">
+        <div className="bannerContent">
+          <h2>Try hosting</h2>
+          <p>Earn extra income and unlock new opportunities by sharing your space.</p>
+          <button>Learn more</button>
+        </div>
+        <img src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=1440" alt="Try hosting"/>
+      </div>
+    </div>
+  )
+}
+
+export default Hosting;
