e27947194461f5a98ed5cba558712119bcd3ee57
Grady Ku <gkupdx@gmail.com>
2021-11-09 17:10:11 -0800

    Update Hosting.js
    - replace placeholder image with different image
    - update banner text

--- src/components/Hosting.js
+++ src/components/Hosting.js
@@ -5,11 +5,10 @@ const Hosting = () => {
     <div className="hosting">
       <div className="hostingWrapper">
         <div className="bannerContent">
-          <h2>Try hosting</h2>
-          <p>Earn extra income and unlock new opportunities by sharing your space.</p>
-          <button>Learn more</button>
+          <h2>Questions about hosting?</h2>
+          <button>Ask a Superhost</button>
         </div>
-        <img src="https://a0.muscache.com/im/pictures/2595054e-d1d9-4fde-8046-58d51fcb3164.jpg?im_w=1440" alt="Try hosting"/>
+        <img src="https://images.unsplash.com/photo-1551883710-e46e63373b50?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80" alt="Questions about hosting?"/>
       </div>
     </div>
   )
