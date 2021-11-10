8ef90d3c72521df14fa4165f76e3bff0a09ec4e4
Grady Ku <gkupdx@gmail.com>
2021-11-09 17:14:19 -0800

    Update Discover.js
    - remove 'Outdoor' collection' panel
    - update banner texts
    - add new buttons

--- src/components/Discover.js
+++ src/components/Discover.js
@@ -3,22 +3,21 @@
 const Discover = () => {
   return (
     <div className="discover">
-      <h2>Discover things to do</h2>
+      <h1>Discover Airbnb Experiences</h1>
       <div className="discoverWrapper">
-        <div>
+        <div className="experiences">
+          <div className="experiencesContent">
+            <h3>Things to do on your trip</h3>
+            <button>Experiences</button>
+          </div>
           <img src="https://a0.muscache.com/im/pictures/ad109d56-2421-40cd-98e6-e114160dc85b.jpg?im_w=480" alt="Sheep"/>
-          <h3>Experiences</h3>
-          <p>Find unforgettable activities near you.</p>
         </div>
-        <div>
+        <div className="onlineExperiences">
+          <div className="onlineExperiencesContent">
+            <h3>Things to do from home</h3>
+            <button>Online Experiences</button>
+          </div>
           <img src="https://a0.muscache.com/im/pictures/0ce799cb-7553-4369-be9e-d0011e0ef636.jpg?im_w=480" alt="Tablet"/>
-          <h3>Online Experiences</h3>
-          <p>Live, interactive activities led by Hosts.</p>
-        </div>
-        <div>
-          <img src="https://a0.muscache.com/im/pictures/3c2676df-0874-45a3-a82f-bbf57ccde1cc.jpg?im_w=480" alt="Canoe"/>
-          <h3>Outdoor collection</h3>
-          <p>Experiences that immerse you in nature.</p>
         </div>
       </div>
     </div>
