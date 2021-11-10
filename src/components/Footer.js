5d2615b9bfdaa6043f81be40056e1478f5f1f83b
Grady Ku <gkupdx@gmail.com>
2021-11-09 17:11:58 -0800

    Update Footer.js
    - minor refactoring

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,56 +1,58 @@
 //// Footer.js - for the footer links
 
 const Footer = () => {
+
   return (
     <div className="footer">
       <div className="footerWrapper">
         <div className="about">
-          <h2>ABOUT</h2>
-          <a href="#link">How Airbnb works</a>
-          <a href="#link">Newsroom</a>
-          <a href="#link">Airbnb 2021</a>
-          <a href="#link">Investors</a>
-          <a href="#link">Airbnb Plus</a>
-          <a href="#link">Airbnb Luxe</a>
-          <a href="#link">HotelTonight</a>
-          <a href="#link">Airbnb for Work</a>
-          <a href="#link">Made possible by Hosts</a>
-          <a href="#link">Careers</a>
-          <a href="#link">Founders' Letter</a>
+          <h2>About</h2>
+          <a href="#how-airbnb-works">How Airbnb works</a>
+          <a href="#newsroom">Newsroom</a>
+          <a href="#2021">Airbnb 2021</a>
+          <a href="#investors">Investors</a>
+          <a href="#airbnb-plus">Airbnb Plus</a>
+          <a href="#airbnb-luxe">Airbnb Luxe</a>
+          <a href="#hotel-tonight">HotelTonight</a>
+          <a href="#airbnb-for-work">Airbnb for Work</a>
+          <a href="#youtube/airbnb">Made possible by Hosts</a>
+          <a href="#careers">Careers</a>
+          <a href="#what-makes-airbnb-airbnb">Founders' Letter</a>
         </div>
         <div className="community">
-          <h2>COMMUNITY</h2>
-          <a href="link">Diversity & Belonging</a>
-          <a href="link">Against Discrimination</a>
-          <a href="link">Accessbility</a>
-          <a href="link">Airbnb Associates</a>
-          <a href="link">Host Afghan refugees</a>
-          <a href="link">Guest Referrals</a>
-          <a href="link">Gift cards</a>
-          <a href="link">Airbnb.org</a>
+          <h2>Community</h2>
+          <a href="#diversity">Diversity & Belonging</a>
+          <a href="#accessibility">Accessibility</a>
+          <a href="#associates">Airbnb Associates</a>
+          <a href="#refugees">Host Afghan refugees</a>
+          <a href="#invite">Guest Referrals</a>
+          <a href="#giftcards">Gift cards</a>
+          <a href="#locale">Airbnb.org</a>
         </div>
         <div className="host">
-          <h2>HOST</h2>
-          <a href="link">Host your home</a>
-          <a href="link">Host an Online Experience</a>
-          <a href="link">Host an Experience</a>
-          <a href="link">Responsible hosting</a>
-          <a href="link">Resource Center</a>
-          <a href="link">Community Center</a>
+          <h2>Host</h2>
+          <a href="#host">Host your home</a>
+          <a href="#onlinehost">Host an Online Experience</a>
+          <a href="#experiences">Host an Experience</a>
+          <a href="#responsible-hosting">Responsible hosting</a>
+          <a href="#resources">Resource Center</a>
+          <a href="#community">Community Center</a>
         </div>
         <div className="support">
-          <h2>SUPPORT</h2>
-          <a href="link">Our COVID-19 Response</a>
-          <a href="link">Help Center</a>
-          <a href="link">Cancellation options</a>
-          <a href="link">Neighborhood Support</a>
-          <a href="link">Trust & Safety</a>
+          <h2>Support</h2>
+          <a href="#covidsafety">Our COVID-19 Response</a>
+          <a href="#help">Help Center</a>
+          <a href="#cancellation">Cancellation options</a>
+          <a href="#neighbors">Neighborhood Support</a>
+          <a href="#trust">Trust & Safety</a>
         </div>
        </div>
 
+      <div className="divider2"></div>
+
       <div className="copyrightWrapper">
         <div className="copyright">
-          <p>&copy; 2021 Airbnb, Inc. &#183; <a href="#link">Privacy</a> &#183; <a href="#link">Terms</a> &#183; <a href="#link">Sitemap</a></p>
+          <p>&copy; 2021 Airbnb, Inc. &#183; <a href="#privacy">Privacy</a> &#183; <a href="#terms-and-conditions">Terms</a> &#183; <a href="#sitemap">Sitemap</a></p>
         </div>
         <div className="social">
           <button><i class="fas fa-globe"></i> English (US)</button>
