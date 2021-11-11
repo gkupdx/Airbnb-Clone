e0dd86b0910552820cea4c05cfb42a16c733c6c1
Grady Ku <gkupdx@gmail.com>
2021-11-11 12:24:20 -0800

    Refactor LiveAnywhere.js to GiftCards.js
    - delete LiveAnywhere.js, and add new GiftCards.js in its place

---
+++ src/components/GiftCards.js
@@ -0,0 +1,18 @@
+//// GiftCards.js - 'Gift cards' section
+
+const GiftCards = () => {
+  return (
+    <div className="giftCards">
+      <div className="giftCardsWrapper">
+        <div className="giftCardContent">
+          <p>Introducing</p>
+          <h2>Airbnb gift cards</h2>
+          <button>Shop now</button>
+        </div>
+        <img src="https://images.pexels.com/photos/264787/pexels-photo-264787.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" alt="Gift cards"/>
+      </div>
+    </div>
+  )
+}
+
+export default GiftCards;
