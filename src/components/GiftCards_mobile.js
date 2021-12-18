746593d0da24e8a44afd9ec0eab9f41bf6e9a81d
Grady Ku <gkupdx@gmail.com>
2021-12-17 17:32:23 -0800

    Add GiftCards_mobile.js
    - add new GiftCards_mobile.js component that serves as the mobile view 
    equivalent of GiftCards.js

---
+++ src/components/GiftCards_mobile.js
@@ -0,0 +1,15 @@
+//// GiftCards_mobile.js
+
+const GiftCards = () => {
+  return (
+    <div className="giftCards">
+      <div className="imageDiv">
+        <p>Introducing</p>
+        <h3>Airbnb gift cards</h3>
+        <button>Shop now</button>
+      </div>
+    </div>
+  )
+}
+
+export default GiftCards;
