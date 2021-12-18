e90f8e6ec8c48db123bc00f9a22bd995f1914988
Grady Ku <gkupdx@gmail.com>
2021-12-17 17:33:13 -0800

    Add Hosting_mobile.js
    - add new Hosting_mobile.js component that serves as the mobile view 
    equivalent of Hosting.js

---
+++ src/components/Hosting_mobile.js
@@ -0,0 +1,14 @@
+//// Hosting_mobile.js
+
+const Hosting = () => {
+  return (
+    <div className="hosting">
+      <div className="hostingBanner">
+        <h2>Questions about hosting?</h2>
+        <button>Ask a Superhost</button>
+      </div>
+    </div>
+  )
+}
+
+export default Hosting;
