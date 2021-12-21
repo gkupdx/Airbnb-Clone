e1803c1b8f1ecb3d543bf0456c36abe7a1fbf809
Grady Ku <gkupdx@gmail.com>
2021-12-21 15:32:53 -0800

    Update NextTrip_mobile.js
    - add new functionality to left & right navigation buttons of carousel
    - add new styling for navigation buttons

--- src/components/NextTrip_mobile.js
+++ src/components/NextTrip_mobile.js
@@ -5,29 +5,71 @@ import { BiChevronLeft } from 'react-icons/bi';
 import { BiChevronRight } from 'react-icons/bi';
 
 const NextTrip = () => {
-  const [clickable, setClickable] = useState(true);
+  const [leftButton, setLeftButton] = useState(false); // toggle left chevron CSS
+  const [rightButton, setRightButton] = useState(true); // toggle right chevron CSS
+  const [position, setPosition] = useState(0);
+  const [updateStyle, setUpdateStyle] = useState({
+    transform: `translateX(${position}px)`,
+    transition: `0.4s ease`
+  })
 
   const chevronIconStyle = {
     fontSize: "18px"
   }
 
-  const shiftRight = {
-    if (clickable) {
-      setClickable(false);
+  // NextTrip carousel
+  const shiftCarousel = (direction) => {
+    // for keeping track of the current carousel position
+    let updatePos = position;
+
+    // DIRECTION - LEFT
+    if (direction === 'left') {
+      if (position === 0 || position === -250) { // check to see if in default position or near default position
+        updatePos = 0;
+        setLeftButton(false);
+      } else if (position <= -600) { // we are near the end of carousel, shift only by 100
+        updatePos += 100;
+        setRightButton(true);
+      } else {
+        updatePos += 250;
+        setLeftButton(true);
+        setRightButton(true);
+      }
+    } else { // DIRECTION - RIGHT
+      if (position < -500) { // we are AT THE END so DO NOT SHIFT RIGHT
+        updatePos -= 0;
+      } else if (position <= -500) { // check to see if we are shifting from near the end of carousel
+        updatePos -= 100;
+        setRightButton(false);
+      } else {
+        updatePos -= 250;
+        setLeftButton(true);
+        setRightButton(true);
+      }
     }
+
+    // shift to the updated position
+    setPosition(updatePos);
+
+    // apply the new style
+    setUpdateStyle({
+      transform: `translateX(${updatePos}px)`,
+      transition: `0.4s ease`
+    });
   }
+
 
   return (
     <div className="nextTrip">
       <div className="flexRow">
         <div><h2>Inspiration for your next trip</h2></div>
         <div className="chevronBtns">
-          <button className={clickable ? "notClickableBtn" : "clickableBtn"}><BiChevronLeft style={chevronIconStyle}/></button>
-          <button onClick={shiftRight} className="clickableBtn"><BiChevronRight style={chevronIconStyle}/></button>
+          <button onClick={() => shiftCarousel('left')} className={leftButton ? "clickableBtn" : "notClickableBtn"}><BiChevronLeft style={chevronIconStyle}/></button>
+          <button onClick={() => shiftCarousel('right')} className={rightButton ? "clickableBtn" : "notClickableBtn"}><BiChevronRight style={chevronIconStyle}/></button>
         </div>
       </div>
 
-      <div className="flexRow">
+      <div style={updateStyle} className="flexRow">
         <div className="flexCol">
           <img src="https://a0.muscache.com/im/pictures/e68a0a87-e41a-489e-a03e-e11236d9b679.jpg?im_w=320" alt="Portland"/>
           <div className="portland">
