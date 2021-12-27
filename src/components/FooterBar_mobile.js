77ad690a4b506649211903deac91be1072ffdfd7
Grady Ku <gkupdx@gmail.com>
2021-12-27 14:23:53 -0800

    Add FooterBar_mobile.js
    - add new FooterBar_mobile.js component that displays a sticky footer 
    bar with additional options for the user

---
+++ src/components/FooterBar_mobile.js
@@ -0,0 +1,63 @@
+//// FooterBar_mobile.js
+
+import { useState } from 'react';
+import { BiSearch, BiHeart } from 'react-icons/bi';
+import { FaUserCircle } from 'react-icons/fa';
+
+const FooterBar = ({ style }) => {
+  const [explore, setExplore] = useState(true);
+  const [wishlist, setWishlist] = useState(false);
+  const [login, setLogin] = useState(false);
+
+  const activeIconStyle = {
+    color: "red",
+    fontSize: "30px"
+  }
+
+  const inactiveIconStyle = {
+    color: "#000",
+    fontSize: "30px"
+  }
+
+  const toggleExplore = () => {
+    if (!explore) {
+      setExplore(!explore);
+      if (login || wishlist) {
+        setLogin(false);
+        setWishlist(false);
+      }
+    }
+  }
+
+  const toggleWishlist = () => {
+    if (!wishlist) {
+      setWishlist(!wishlist);
+      if (explore || login) {
+        setExplore(false);
+        setLogin(false);
+      }
+    }
+  }
+
+  const toggleLogin = () => {
+    if (!login) {
+      setLogin(!login);
+      if (explore || wishlist) {
+        setExplore(false);
+        setWishlist(false);
+      }
+    }
+  }
+
+  return (
+    <div style={style} className="footerBarSticky">
+      <div className="footerBarBtns">
+        {explore ? <a onClick={toggleExplore} className="flexCol activeFooterLink" href="#explore"><BiSearch style={activeIconStyle}/>Explore</a> : <a onClick={toggleExplore} className="flexCol inactiveFooterLink" href="#explore"><BiSearch style={inactiveIconStyle}/>Explore</a>}
+        {wishlist ? <a onClick={toggleWishlist} className="flexCol activeFooterLink" href="#wishlist"><BiHeart style={activeIconStyle}/>Wishlists</a> : <a onClick={toggleWishlist} className="flexCol inactiveFooterLink" href="#wishlist"><BiHeart style={inactiveIconStyle}/>Wishlists</a>}
+        {login ? <a onClick={toggleLogin} className="flexCol activeFooterLink" href="#login"><FaUserCircle style={activeIconStyle}/>Log in</a> : <a onClick={toggleLogin} className="flexCol inactiveFooterLink" href="#login"><FaUserCircle style={inactiveIconStyle}/>Log in</a>}
+      </div>
+    </div>
+  )
+}
+
+export default FooterBar;
