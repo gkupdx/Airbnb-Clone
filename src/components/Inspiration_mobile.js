f2f320609fa734c7613141d95c3f841304b57e28
Grady Ku <gkupdx@gmail.com>
2022-01-03 12:10:11 -0800

    Update Inspiration_mobile.js
    - import new icons and components
    - add new styling to various elements
    - add functionality to 'Show more' button
    - add functionality to left & right navigation caret buttons
    - combine separate onClick() functions into one function that takes in 
    an argument

--- src/components/Inspiration_mobile.js
+++ src/components/Inspiration_mobile.js
@@ -1,9 +1,20 @@
 //// Inspiration_mobile.js
 
 import { useState } from 'react';
+import { BiChevronLeft, BiChevronRight } from 'react-icons/bi';
 import ArtLocation from './ArtLocation_mobile';
+import OutdoorLocation from './OutdoorLocation_mobile';
+import CabinsLocation from './CabinsLocation_mobile';
+import BeachLocation from './BeachLocation_mobile';
+import PopularLocation from './PopularLocation_mobile';
+import UniqueLocation from './UniqueLocation_mobile';
 
 const Inspiration = () => {
+  const [showSeeMore, setShowSeeMore] = useState(true); // toggle state for showing/hiding 'See more' button
+  const [hiddenStyle, setHiddenStyle] = useState({ // CSS toggle for showing/hiding additional locations
+    display: "none"
+  });
+
   const [art, setArt] = useState(true);
   const [outdoor, setOutdoor] = useState(false);
   const [cabins, setCabins] = useState(false);
@@ -11,125 +22,204 @@ const Inspiration = () => {
   const [popular, setPopular] = useState(false);
   const [unique, setUnique] = useState(false);
 
-  const displayArt = () => {
-    if (!art) {
-      setArt(!art);
-      if (outdoor || cabins || beach || popular || unique) {
-        setOutdoor(false);
-        setCabins(false);
-        setBeach(false);
-        setPopular(false);
-        setUnique(false);
+  const [leftButton, setLeftButton] = useState(false);
+  const [rightButton, setRightButton] = useState(true);
+  const [position, setPosition] = useState(0);
+  const [updateStyle, setUpdateStyle] = useState({
+    transform: `translateX(${position}px)`
+  });
+
+  // Passed-in prop to _Location.js components
+  // to stylize the country name to a lower opacity
+  const lightText = {
+    color: "rgba(0, 0, 0, 0.6)"
+  }
+
+  // Passed-in prop to _Location.js components
+  // to stylize the 'Show more' button
+  const showMoreStyle = {
+    fontWeight: 600,
+    textDecoration: "underline"
+  }
+
+  // Passed-in prop to _Location.js components
+  // for 'Show more' button onClick functionality
+  const showMoreLocations = () => {
+    setHiddenStyle({
+      display: "block"
+    });
+    setShowSeeMore(false);
+  }
+
+  // Inspiration navigation chevron icon
+  const chevronIconStyle = {
+    fontSize: "25px"
+  }
+
+  // Displays correct locations panel on
+  // corresponding button click
+  const displayPanel = (button) => {
+    if (button === 'art') {
+      if (!art) {
+        setArt(!art);
+        if (outdoor || cabins || beach || popular || unique) {
+          setOutdoor(false);
+          setCabins(false);
+          setBeach(false);
+          setPopular(false);
+          setUnique(false);
+        }
+      }
+    }
+
+    if (button === 'outdoor') {
+      if (!outdoor) {
+        setOutdoor(!outdoor);
+        if (art || cabins || beach || popular || unique) {
+          setArt(false);
+          setCabins(false);
+          setBeach(false);
+          setPopular(false);
+          setUnique(false);
+        }
+      }
+    }
+
+    if (button === 'cabins') {
+      if (!cabins) {
+        setCabins(!cabins);
+        if (art || outdoor || beach || popular || unique) {
+          setArt(false);
+          setOutdoor(false);
+          setBeach(false);
+          setPopular(false);
+          setUnique(false);
+        }
+      }
+    }
+
+    if (button === 'beach') {
+      if (!beach) {
+        setBeach(!beach);
+        if (art || outdoor || cabins || popular || unique) {
+          setArt(false);
+          setOutdoor(false);
+          setCabins(false);
+          setPopular(false);
+          setUnique(false);
+        }
+      }
+    }
+
+    if (button === 'popular') {
+      if (!popular) {
+        setPopular(!popular);
+        if (art || outdoor || cabins || beach || unique) {
+          setArt(false);
+          setOutdoor(false);
+          setCabins(false);
+          setBeach(false);
+          setUnique(false);
+        }
+      }
+    }
+
+    if (button === 'unique') {
+      if (!unique) {
+        setUnique(!unique);
+        if (art || outdoor || cabins || beach || popular) {
+          setArt(false);
+          setOutdoor(false);
+          setCabins(false);
+          setBeach(false);
+          setPopular(false);
+        }
       }
     }
   }
 
-  const displayOutdoor = () => {
-    if (!outdoor) {
-      setOutdoor(!outdoor);
-      if (art || cabins || beach || popular || unique) {
-        setArt(false);
-        setCabins(false);
-        setBeach(false);
-        setPopular(false);
-        setUnique(false);
-      }
-    }
-  }
+  // Inspiration nav button functionality (MOBILE VIEW)
+  const shiftButtons = (direction) => {
+    let updatePos = position;
 
-  // Displays correct corresponding content when
-  // 'Mountain Cabins' button is clicked
-  const displayCabins = () => {
-    if (!cabins) {
-      setCabins(!cabins);
-      if (art || outdoor || beach || popular || unique) {
-        setArt(false);
-        setOutdoor(false);
-        setBeach(false);
-        setPopular(false);
-        setUnique(false);
-      }
+    // DIRECTION - LEFT
+    if (direction === 'left') {
+      updatePos += 50;
+      setLeftButton(true);
+      setRightButton(true);
+    } else { // DIRECTION - RIGHT
+      updatePos -= 50;
+      setLeftButton(true);
+      setRightButton(true);
     }
-  }
 
-  // Displays correct corresponding content when
-  // 'Beach destinations' button is clicked
-  const displayBeach = () => {
-    if (!beach) {
-      setBeach(!beach);
-      if (art || outdoor || cabins || popular || unique) {
-        setArt(false);
-        setOutdoor(false);
-        setCabins(false);
-        setPopular(false);
-        setUnique(false);
-      }
-    }
-  }
+    // shift <div> to updated position
+    setPosition(updatePos);
 
-  // Displays correct corresponding content when
-  // 'Popular destinations' button is clicked
-  const displayPopular = () => {
-    if (!popular) {
-      setPopular(!popular);
-      if (art || outdoor || cabins || beach || unique) {
-        setArt(false);
-        setOutdoor(false);
-        setCabins(false);
-        setBeach(false);
-        setUnique(false);
-      }
+    // AFTER SHIFT, check to see if we're near the beginning
+    // if yes, hide left button
+    if (updatePos + 50 === 0) {
+      setLeftButton(false);
     }
-  }
+    // AFTER SHIFT, check to see if we're near the end
+    // if yes, hide right button
+    if (updatePos - 50 === -650) {
+      setRightButton(false);
+    }
 
-  // Displays correct corresponding content when
-  // 'Unique Stays' button is clicked
-  const displayUnique = () => {
-    if (!unique) {
-      setUnique(!unique);
-      if (art || outdoor || cabins || beach || popular) {
-        setArt(false);
-        setOutdoor(false);
-        setCabins(false);
-        setBeach(false);
-        setPopular(false);
-      }
-    }
+    // apply the new style (<div>)
+    setUpdateStyle({
+      transform: `translateX(${updatePos}px)`
+    });
   }
 
   return (
     <div className="inspiration">
+      {/* Inspiration left & right nav buttons */ }
+      <div className="navWrapper">
+        <button onClick={() => shiftButtons('left')} className={leftButton ? "showLeftNavBtn" : "hiddenNavBtn"}><BiChevronLeft style={chevronIconStyle}/></button>
+        <button onClick={() => shiftButtons('right')} className={rightButton ? "showRightNavBtn" : "hiddenNavBtn"}><BiChevronRight style={chevronIconStyle}/></button>
+      </div>
+
       <div className="inspirationWrapper">
         <h2>Inspiration for future getaways</h2>
-        <div className="inspirationBtns">
+
+        {/* Inspiration destination buttons */ }
+        <div style={updateStyle} className="inspirationBtnRow">
           <div className="inspirationBtnWrapper">
-            <button onClick={displayArt} className={art ? "activeBtn" : "inactiveBtn"}>Destinations for arts & culture</button>
+            <button onClick={() => displayPanel('art')} className={art ? "activeBtn" : "inactiveBtn"}>Destinations for arts & culture</button>
             <div className={art ? "artBorderBottom" : ""}></div>
           </div>
           <div className="inspirationBtnWrapper">
-            <button onClick={displayOutdoor} className={outdoor ? "activeBtn" : "inactiveBtn"}>Destinations for outdoor adventure</button>
+            <button onClick={() => displayPanel('outdoor')} className={outdoor ? "activeBtn" : "inactiveBtn"}>Destinations for outdoor adventure</button>
             <div className={outdoor ? "outdoorBorderBottom" : ""}></div>
           </div>
           <div className="inspirationBtnWrapper">
-            <button onClick={displayCabins} className={cabins ? "activeBtn" : "inactiveBtn"}>Mountain cabins</button>
+            <button onClick={() => displayPanel('cabins')} className={cabins ? "activeBtn" : "inactiveBtn"}>Mountain cabins</button>
             <div className={cabins ? "cabinsBorderBottom" : ""}></div>
           </div>
           <div className="inspirationBtnWrapper">
-            <button onClick={displayBeach} className={beach ? "activeBtn" : "inactiveBtn"}>Beach destinations</button>
+            <button onClick={() => displayPanel('beach')} className={beach ? "activeBtn" : "inactiveBtn"}>Beach destinations</button>
             <div className={beach ? "beachBorderBottom" : ""}></div>
           </div>
           <div className="inspirationBtnWrapper">
-            <button onClick={displayPopular} className={popular ? "activeBtn" : "inactiveBtn"}>Popular destinations</button>
+            <button onClick={() => displayPanel('popular')} className={popular ? "activeBtn" : "inactiveBtn"}>Popular destinations</button>
             <div className={popular ? "popularBorderBottom" : ""}></div>
           </div>
           <div className="inspirationBtnWrapper">
-            <button onClick={displayUnique} className={unique ? "activeBtn" : "inactiveBtn"}>Unique Stays</button>
+            <button onClick={() => displayPanel('unique')} className={unique ? "activeBtn" : "inactiveBtn"}>Unique Stays</button>
             <div className={unique ? "uniqueBorderBottom" : ""}></div>
           </div>
         </div>
         <div className="divideY2"></div>
 
-        {art ? <ArtLocation /> : ""}
+        {art ? <ArtLocation lightText={lightText} showMoreStyle={showMoreStyle} showSeeMore={showSeeMore} hiddenStyle={hiddenStyle} showMoreLocations={showMoreLocations}/> : ""}
+        {outdoor ? <OutdoorLocation lightText={lightText} showMoreStyle={showMoreStyle} showSeeMore={showSeeMore} hiddenStyle={hiddenStyle} showMoreLocations={showMoreLocations}/> : ""}
+        {cabins ? <CabinsLocation lightText={lightText} showMoreStyle={showMoreStyle} showSeeMore={showSeeMore} hiddenStyle={hiddenStyle} showMoreLocations={showMoreLocations}/> : ""}
+        {beach ? <BeachLocation lightText={lightText} showMoreStyle={showMoreStyle} showSeeMore={showSeeMore} hiddenStyle={hiddenStyle} showMoreLocations={showMoreLocations}/> : ""}
+        {popular ? <PopularLocation lightText={lightText} showMoreStyle={showMoreStyle} showSeeMore={showSeeMore} hiddenStyle={hiddenStyle} showMoreLocations={showMoreLocations}/> : ""}
+        {unique ? <UniqueLocation lightText={lightText}/> : ""}
+
       </div>
     </div>
   )
