4060502007af78090312ed64ccb2cc9a60d3c502
Grady Ku <gkupdx@gmail.com>
2022-01-03 13:00:16 -0800

    Update App.js
    - import useState, useEffect, and FooterBar component
    - add footer bar transition effects based on scroll behavior
    - add function to refresh page to top when browser resized to tablet 
    viewport

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 //// App.js
 
+import { useState, useEffect } from 'react';
 /*import Header from './components/Header';
 import NextTrip from './components/NextTrip';
 import GiftCards from './components/GiftCards';
@@ -14,9 +15,52 @@ import Discover from './components/Discover_mobile';
 import Hosting from './components/Hosting_mobile';
 import Inspiration from './components/Inspiration_mobile';
 import Footer from './components/Footer_mobile';
-//import FooterBar from './components/FooterBar_mobile';
+import FooterBar from './components/FooterBar_mobile';
 
 function App() {
+
+  // footer bar CSS transition into visible
+  const scrollUpStyle = {
+    bottom: "0",
+    transition: "0.1s ease-in"
+  };
+  // footer bar CSS transition into hidden
+  const scrollDownStyle = {
+    bottom: "-65px",
+    transition: "0.1s ease-out"
+  };
+
+  // Function to show or hide footer bar based on scroll position
+  const [currentPos, setCurrentPos] = useState(document.scrollingElement.scrollHeight); // grab the current scroll height
+  const [visible, setVisible] = useState(false);
+
+  const visibleFooterBar = () => {
+    if (currentPos < window.pageYOffset) { // we're scrolling DOWN - HIDE
+      setVisible(false);
+    } else if (currentPos > window.pageYOffset) { // we're scrolling UP - SHOW
+      setVisible(true);
+    }
+
+    // update current Y position to new scroll position
+    setCurrentPos(window.pageYOffset);
+  }
+
+  useEffect(() => {
+    visibleFooterBar()
+    window.addEventListener("scroll", visibleFooterBar)
+  });
+
+  // Function to refresh page to top at tablet width breakpoint
+  const forceRefresh = () => {
+    if (window.innerWidth === 900) {
+      window.scrollTo(0, 0);
+    }
+  }
+
+  useEffect(() => {
+    forceRefresh()
+    window.addEventListener("resize", forceRefresh)
+  });
 
   return (
     <div className="container">
@@ -34,7 +78,10 @@ function App() {
       <Hosting />
       <Inspiration />
       <Footer />
-      {/*<FooterBar />*/}
+      {/* check to see if pageYOffset (px) is near bottom of page */}
+      {/* if YES, hide footer bar */}
+      {/* if NO, determine if we're scrolling up or down */}
+      {window.pageYOffset <= 4000 ? visible ? <FooterBar style={scrollUpStyle}/> : <FooterBar style={scrollDownStyle}/> : ""}
     </div>
   );
 }
