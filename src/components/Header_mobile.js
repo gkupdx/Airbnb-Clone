c8ae911a45e026bfdd07f55e80562c168a3a4ecd
Grady Ku <gkupdx@gmail.com>
2022-01-17 12:35:14 -0800

    Update Header_mobile.js
    - add new onClick prop (temporarily unused)

--- src/components/Header_mobile.js
+++ src/components/Header_mobile.js
@@ -2,13 +2,14 @@
 
 import GiftCards from './GiftCards_mobile';
 
-const Header = () => {
+const Header = ({ onClick }) => {
+
   return (
     <div className="header">
       <div className="heroImage">
         <div className="heroImageDiv">
           <h3>Not sure where to go? Perfect.</h3>
-          <button className="flexibleBtn"><p>I'm flexible</p></button>
+          <button><p>I'm flexible</p></button>
         </div>
       </div>
 
