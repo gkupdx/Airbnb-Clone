9b50f8b3ecc1cce519b26f449aaf922d087ae5ef
Grady Ku <gkupdx@gmail.com>
2022-01-18 14:23:52 -0800

    Update SearchBar_mobile.js
    - import new icons
    - import new Modal package
    - reduce browser width threshold from 900px to 850px
    - add new modal open & close functions
    - render new Modal component (temporarily commented out)

--- src/components/SearchBar_mobile.js
+++ src/components/SearchBar_mobile.js
@@ -1,12 +1,15 @@
 //// SearchBar_mobile.js
 
 import { useState, useEffect } from 'react';
-import { BiSearch } from 'react-icons/bi';
+import { BiSearch, BiChevronLeft } from 'react-icons/bi';
+import { BsChevronRight } from 'react-icons/bs';
+import Modal from 'react-modal';
 
 import TabletBar from './TabletBar';
 import TabletBarSticky from './TabletBarSticky';
 
 const SearchBar = () => {
+  const [modalState, setModalState] = useState(false);
   const [sticky, setSticky] = useState(true);
   const [tabletView, setTabletView] = useState(false);
 
@@ -16,7 +19,15 @@ const SearchBar = () => {
     marginRight: "5px"
   }
 
-  // Transition from fixed to "sticky" search bar on window scroll
+  const chevronRightStyle = {
+    fontSize: "25px",
+    fontWeight: "600",
+    backgroundImage: "linear-gradient(to right, #4d0585, #b3277f)",
+    backgroundClip: "text",
+    WebkitTextFillColor: "transparent"
+  }
+
+  // Transition from FIXED to STICKY search bar on window scroll
   const stickySearchBar = () => {
     if (window.pageYOffset === 0) {
       setSticky(false);
@@ -24,7 +35,7 @@ const SearchBar = () => {
       setSticky(true);
     }
   }
-    /* paired with 'stickySearchBar()' */
+
   useEffect(() => {
     stickySearchBar()
     window.addEventListener("scroll", stickySearchBar)
@@ -32,7 +43,7 @@ const SearchBar = () => {
 
   // Transition from MOBILE to TABLET search bar on window resize (dynamic)
   const tabletSearchBar = () => {
-    if (window.innerWidth >= 900) {
+    if (window.innerWidth >= 850) {
       setTabletView(true);
     } else {
       setTabletView(false);
@@ -44,19 +55,37 @@ const SearchBar = () => {
     window.addEventListener("resize", tabletSearchBar)
   })
 
+  // Open modal on search bar click
+  const openModal = () => {
+    setModalState(true);
+  }
+
+  // Close modal on chevron button click
+  const closeModal = () => {
+    setModalState(false);
+  }
+
   return (
     <>
     {!tabletView ?
-      // browser width LESS than 900px ---> display MOBILE search bar
+      // browser width LESS than 850px ---> display MOBILE search bar
       <div className={!sticky ? "fixedSearchBar" : "stickySearchBar"}>
         <button className={!sticky ? "searchBtn" : "searchBtn2"}><BiSearch style={searchIconStyle}/>Where are you going?</button>
       </div>
       :
-      // browser width GREATER/EQUAL to 900px ---> display TABLET search bar
+      // browser width GREATER/EQUAL to 850px ---> display TABLET search bar
       <div className={!sticky ? "fixedSearchBar" : "stickySearchBar"}>
         {!sticky ? <TabletBar /> : <TabletBarSticky />}
       </div>
     }
+
+      {/* Search bar onClick modal */}
+      <Modal className={modalState ? "openModal" : "closedModal"} isOpen={modalState}>
+        <button className="closeModalBtn" onClick={closeModal}><BiChevronLeft /></button>
+        <input className="inputBtn" type="text" placeholder="Where are you going?"/>
+        <p className="flavorText">GO ANYWHERE, ANYTIME</p>
+        <button className="flexibleBtn"><p>I'm flexible</p><BsChevronRight style={chevronRightStyle}/></button>
+      </Modal>
     </>
   )
 }
