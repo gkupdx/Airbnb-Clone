de29b244f332ac09df035d4a56d3c462ce0f7601
Grady Ku <gkupdx@gmail.com>
2022-01-18 14:21:33 -0800

    Update NextTrip.js
    - add ref to card element for dynamic resizing based on browser width
    - refactor shiftCarousel() to account for more edge cases
    - add conditional rendering of elements based on browser width

--- src/components/NextTrip_mobile.js
+++ src/components/NextTrip_mobile.js
@@ -1,14 +1,16 @@
 //// NextTrip_mobile.js
 
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import { BiChevronLeft } from 'react-icons/bi';
 import { BiChevronRight } from 'react-icons/bi';
 
 const NextTrip = () => {
+  const cardWidth = useRef(); // ref to grab width of dynamic card elements
+
   const [leftButton, setLeftButton] = useState(false); // toggle left chevron CSS
   const [rightButton, setRightButton] = useState(true); // toggle right chevron CSS
-  const [position, setPosition] = useState(0);
-  const [updateStyle, setUpdateStyle] = useState({
+  const [position, setPosition] = useState(0); // keep track of X-coordinate of <div>
+  const [updateStyle, setUpdateStyle] = useState({ // apply new style
     transform: `translateX(${position}px)`,
     transition: `0.4s ease`
   });
@@ -17,38 +19,68 @@ const NextTrip = () => {
     fontSize: "18px"
   }
 
-  // NextTrip carousel
+  // NextTrip carousel nav buttons
   const shiftCarousel = (direction) => {
-    // for keeping track of the current carousel position
-    let updatePos = position;
+    let updatePos = position; // keeping track of the current carousel position
 
     // DIRECTION - LEFT
     if (direction === 'left') {
-      if (position === 0 || position === -240) { // check to see if in default position or near default position
+      if (position === 0 || position + cardWidth.current.offsetWidth + 20 >= 0) { // check if IN default position or 1 away from default
         updatePos = 0;
         setLeftButton(false);
-      } else if (position <= -500) { // we are near the end of carousel, shift only by 100
-        updatePos += 100;
+
+      } else if (position <= -(cardWidth.current.offsetWidth * 2.4)) { // shifting LEFT from near end of carousel, shift only 2.5x
+        updatePos += cardWidth.current.offsetWidth / 2.5;
         setRightButton(true);
+
       } else {
-        updatePos += 240;
+        updatePos += (cardWidth.current.offsetWidth + 20); // add 20 to take into account margin-right value
         setLeftButton(true);
         setRightButton(true);
       }
-    } else { // DIRECTION - RIGHT
-      if (position < -480) { // we are AT THE END so DO NOT SHIFT RIGHT
+
+    } else if (direction === 'right'){ // DIRECTION - RIGHT
+      if (position <= -(cardWidth.current.offsetWidth * 2.5)) { // we are AT THE END so DO NOT SHIFT RIGHT
         updatePos -= 0;
-      } else if (position <= -480) { // check to see if we are shifting from near the end of carousel
-        updatePos -= 100;
+
+      } else if (position - cardWidth.current.offsetWidth <= -(cardWidth.current.offsetWidth * 3)) { // shifting RIGHT at near end of carousel, shift only 2.5x
+        updatePos -= cardWidth.current.offsetWidth / 2.5;
+        setRightButton(false);
+
+      } else {
+        updatePos -= (cardWidth.current.offsetWidth + 20); // add 20 to take into account margin-right value
+        setLeftButton(true);
+        setRightButton(true);
+      }
+
+    } else if (direction === 'leftTab') { // DIRECTION - LEFT [tablet]
+      if (position === 0 || position + cardWidth.current.offsetWidth + 20 >= 0) {
+        updatePos = 0;
+        setLeftButton(false);
+      } else if (position <= -(cardWidth.current.offsetWidth * 1.5)) {
+        updatePos += cardWidth.current.offsetWidth / 1.5;
+        setRightButton(true);
+      } else {
+        updatePos += (cardWidth.current.offsetWidth + 20);
+        setLeftButton(true);
+        setRightButton(true);
+      }
+
+    } else if (direction === 'rightTab') { // DIRECTION - RIGHT [tablet]
+      if (position <= -(cardWidth.current.offsetWidth * 1.5)) {
+        updatePos -= 0;
+      } else if (position - cardWidth.current.offsetWidth <= -(cardWidth.current.offsetWidth * 2)) {
+        updatePos -= cardWidth.current.offsetWidth / 1.5;
         setRightButton(false);
       } else {
-        updatePos -= 240;
+        updatePos -= (cardWidth.current.offsetWidth + 20);
         setLeftButton(true);
         setRightButton(true);
       }
     }
 
-    // shift to the updated position
+
+    // shift to updated position
     setPosition(updatePos);
 
     // apply the new style
@@ -63,13 +95,21 @@ const NextTrip = () => {
       <div className="flexRow">
         <div><h2>Inspiration for your next trip</h2></div>
         <div className="chevronBtns">
-          <button onClick={() => shiftCarousel('left')} className={leftButton ? "clickableBtn" : "notClickableBtn"}><BiChevronLeft style={chevronIconStyle}/></button>
-          <button onClick={() => shiftCarousel('right')} className={rightButton ? "clickableBtn" : "notClickableBtn"}><BiChevronRight style={chevronIconStyle}/></button>
+          {window.innerWidth < 850 ?
+          <>
+            <button onClick={() => shiftCarousel('left')} className={leftButton ? "clickableBtn" : "notClickableBtn"}><BiChevronLeft style={chevronIconStyle}/></button>
+            <button onClick={() => shiftCarousel('right')} className={rightButton ? "clickableBtn" : "notClickableBtn"}><BiChevronRight style={chevronIconStyle}/></button>
+          </>
+          :
+          <>
+            <button onClick={() => shiftCarousel('leftTab')} className={leftButton ? "clickableBtn" : "notClickableBtn"}><BiChevronLeft style={chevronIconStyle}/></button>
+            <button onClick={() => shiftCarousel('rightTab')} className={rightButton ? "clickableBtn" : "notClickableBtn"}><BiChevronRight style={chevronIconStyle}/></button>
+          </>}
         </div>
       </div>
 
       <div style={updateStyle} className="flexRowCards">
-        <div className="mtHoodCard"></div>
+        <div ref={cardWidth} className="mtHoodCard"></div>
         <div className="sunriverCard"></div>
         <div className="leavenworthCard"></div>
         <div className="lincolnCityCard"></div>
